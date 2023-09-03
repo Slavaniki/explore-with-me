621607dfe5a708adcfe9aa0f0e32dc9e55e9a158
Slavaniki <slavaniki2@yandex.ru>
2023-09-03 22:16:19 +0300

    return query

--- stats-service/src/main/java/ru/practicum/explorewithme/service/StatsRepository.java
+++ stats-service/src/main/java/ru/practicum/explorewithme/service/StatsRepository.java
@@ -1,13 +1,15 @@
 package ru.practicum.explorewithme.service;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.practicum.explorewithme.model.EndpointHit;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
+    @Query("select e from EndpointHit e where e.timestamp > :start and e.timestamp < :end and e.uri in :uris")
     List<EndpointHit> findByUriInAndTimestampAfterAndTimestampBefore(List<String> uris, LocalDateTime start, LocalDateTime end);
-
+    @Query("select e from EndpointHit e where e.timestamp > :start and e.timestamp < :end")
     List<EndpointHit> findByTimestampAfterAndTimestampBefore(LocalDateTime start, LocalDateTime end);
 }
