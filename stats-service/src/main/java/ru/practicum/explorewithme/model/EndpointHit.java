55c015b04a70c618f4c9e645970807203768bc26
Slavaniki <slavaniki2@yandex.ru>
2023-09-05 10:17:47 +0300

    fix step 1

--- stats-service/src/main/java/ru/practicum/explorewithme/model/EndpointHit.java
+++ stats-service/src/main/java/ru/practicum/explorewithme/model/EndpointHit.java
@@ -4,25 +4,17 @@ import lombok.*;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
+@Data
 @Table(name = "endpoint_hit")
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
-@EqualsAndHashCode
 public class EndpointHit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Integer id;
-    @Column(name = "app")
     private String app;
-    @Column(name = "uri")
     private String uri;
-    @Column(name = "ip")
     private String ip;
-    @Column(name = "timestamp")
     private LocalDateTime timestamp;
 }
