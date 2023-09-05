f49e84aeb1d31753a016383ec79eaa15afdefb57
Slavaniki <slavaniki2@yandex.ru>
2023-09-05 10:29:00 +0300

    fix review change query params

--- stats-service/src/main/java/ru/practicum/explorewithme/service/StatsRepository.java
+++ stats-service/src/main/java/ru/practicum/explorewithme/service/StatsRepository.java
@@ -1,6 +1,7 @@
 package ru.practicum.explorewithme.service;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.practicum.explorewithme.model.EndpointHit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,9 +9,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
-    @Query("select e from EndpointHit e where e.timestamp > ?1 and e.timestamp < ?2 and e.uri in ?3")
-    List<EndpointHit> findEndpointHitsByUrisAndStartBeforeAndEndAfter(LocalDateTime start, LocalDateTime end, List<String> uris);
+    @Query("select e from EndpointHit e where e.timestamp > :start and e.timestamp < :end and e.uri in :uris")
+    List<EndpointHit> findEndpointHitsByUrisAndStartBeforeAndEndAfter(@Param("start") LocalDateTime start,
+                                                                      @Param("end") LocalDateTime end,
+                                                                      @Param("uris") List<String> uris);
 
-    @Query("select e from EndpointHit e where e.timestamp > ?1 and e.timestamp < ?2")
-    List<EndpointHit> findEndpointHitsByUrisAndStartBeforeAndEndAfter(LocalDateTime start, LocalDateTime end);
+    @Query("select e from EndpointHit e where e.timestamp > :start and e.timestamp < :end")
+    List<EndpointHit> findEndpointHitsByUrisAndStartBeforeAndEndAfter(@Param("start") LocalDateTime start,
+                                                                      @Param("end") LocalDateTime end);
 }
