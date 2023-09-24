11947808a2d2c8dd386f73451d0d869547927e9b
Slavaniki <slavaniki2@yandex.ru>
2023-09-25 02:49:02 +0300

    3

--- stats-service/src/main/java/ru/practicum/explorewithme/model/Triple.java
+++ stats-service/src/main/java/ru/practicum/explorewithme/model/Triple.java
@@ -11,15 +11,15 @@ public class Triple<T, U, V> {
         this.third = third;
     }
 
-    public T getFirst() {
-        return first;
+    public V getThird() {
+        return third;
     }
-    
+
     public U getSecond() {
         return second;
     }
 
-    public V getThird() {
-        return third;
+    public T getFirst() {
+        return first;
     }
 }
