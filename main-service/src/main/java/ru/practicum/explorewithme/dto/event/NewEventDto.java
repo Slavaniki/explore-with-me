83a8ecfb987e4c3d8914f69ef9e100eca5fbe03d
Slavaniki <slavaniki2@yandex.ru>
2023-10-08 12:26:50 +0300

    fix

--- main-service/src/main/java/ru/practicum/explorewithme/dto/event/NewEventDto.java
+++ main-service/src/main/java/ru/practicum/explorewithme/dto/event/NewEventDto.java
@@ -2,6 +2,8 @@ package ru.practicum.explorewithme.dto.event;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import ru.practicum.explorewithme.model.Location;
+
 import java.time.LocalDateTime;
 
 @Data
@@ -11,7 +13,7 @@ public class NewEventDto {
     private String description;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime eventDate;
-    private EventFullDto.Location location;
+    private Location location;
     private Boolean paid;
     private Integer participantLimit;
     private Boolean requestModeration;
